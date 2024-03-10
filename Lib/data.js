dfb79e3c05a21cf378342d07474129fbda74f39c
limon513 <sabur.limon@sec.ac.bd>
2024-03-10 10:01:49 +0600

    added file read functionality and done test

--- Lib/data.js
+++ Lib/data.js
@@ -1,3 +1,38 @@
 //dependencies
 const fs = require('node:fs');
+const path = require('node:path');
 //scaffolding
+const lib = {};
+//base directory
+lib.baseDir = path.join(__dirname, '../.data/');
+//function to create a file 
+lib.create = (dir, filename, data, callback)=>{
+    fs.open(`${lib.baseDir+dir}/${filename}.json`,'wx',(err,fd)=>{
+        if(!err && fd){
+            const dataToString = JSON.stringify(data);
+            fs.writeFile(fd,dataToString,(err1)=>{
+                if(!err1){
+                    fs.close(fd,(err2)=>{
+                        if(!err2){
+                            callback('file created successfull');
+                        }else{
+                            callback(err2);
+                        }
+                    });
+                }else{
+                    callback(err1);
+                }
+            });
+        }else{
+            callback(err);
+        }
+    });
+};
+//function to read file
+lib.read = (dir, filename, callback)=>{
+    fs.readFile(`${lib.baseDir+dir}/${filename}.json`,'utf-8',(err,data)=>{
+        callback(err,data);
+    });
+};
+
+module.exports = lib;
--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const http = require('node:http');
 const url = require('node:url');
 const {handleReqRes} = require('./Helpers/handleReqRes');
 const environment = require('./Helpers/enviornment');
+const data = require('./Lib/data');
 //app scafolding
 const app = {};
+
+//testing file creation
+data.create('files','test',{'message':'creating test file','author':'pegasus'},(err)=>{
+    if(err) console.log(err);
+    else console.log('file created successfully');
+});
+//testing file read
+data.read('files','test',(err,data)=>{
+    console.log(err);
+    console.log(data);
+});
 //creating server
 app.createServer = () => {
     const server = http.createServer(app.handleRR);
